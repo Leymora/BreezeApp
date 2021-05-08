0cc750604e740015ecc62c395fada37a4c1dfc70
Vilhelm <13785032+SentimentalWoosh@users.noreply.github.com>
2021-05-08 16:59:18 +0200

    Epic

--- main.js
+++ main.js
@@ -5,11 +5,25 @@ function createWindow (){
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
+
+    const popup = new BrowserWindow({
+        width: 100,
+        height: 200,
+        
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        },
+        parent: win,
+        frame: false
+    })
+
     win.loadFile('index.html')
+    popup.loadFile('popup.html')
 }
 
 app.whenReady().then(() => {
